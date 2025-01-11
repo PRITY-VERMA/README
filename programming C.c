7bf6d107004855b42879b2c9713bfd3766f428dc
PRITY-VERMA <pv327591@gmail.com>
2025-01-11 12:58:23 +0530

    Add files via upload

---
+++ programming C.c
@@ -0,0 +1,11 @@
+  #include<stdio.h>
+
+int main(){
+  printf("my all family member");
+    printf("my father's name is surendra kumar verma ");
+    printf("my mother's name is basanti verma");
+    printf("my sister's name is priyanka verma");
+    printf("my name is prity verma");
+    printf("my brother's name is prince verma");
+    return 0;
+}
